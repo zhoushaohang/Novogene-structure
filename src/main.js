9f929b99646c0d0a2e941430c5fe9e11c6fcb15f
zhoushaohang <1181842363@qq.com>
2022-02-23 17:03:54 +0800

    open

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import Antd from 'ant-design-vue';
 import App from './App.vue';
 import 'ant-design-vue/dist/antd.css';
 import router from './router';
+import axios from 'axios';
 
 const app = createApp(App);
 app.use(Antd)
 app.use(router)
 app.mount('#app')
+app.config.globalProperties.$http = axios
