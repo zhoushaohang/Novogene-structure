78906de4caa513908dc52587956a3c15ffa0eea3
周少行 <1181842363@qq.com>
2022-09-29 17:11:17 +0800

    *#*

--- src/home.vue
+++ src/home.vue
@@ -2,20 +2,6 @@
   <router-link to="/index">hello</router-link>
 </template>
 <script>
-export default {
-  name: '',
-  props: {},
-  components: {
-  },
-  data() {
-    return {};
-  },
-  computed: {},
-  watch: {},
-  methods: {},
-  created() {},
-  mounted() {},
-};
 </script>
 <style lang='less' scoped>
 </style>
